4fbd4b88824bfa6c8e6f225d09462847bcd6342b
shoyo-1 <shoyoinokuchi@gmail.com>
2018-11-02 19:34:20 +0900

    ocr OP

--- src/parser.py
+++ src/parser.py
@@ -1,13 +1,32 @@
-import PyPDF2
+from PIL import Image
+import pytesseract
 
-def loadPage(pageNum):
-    pdfFileObj = open('../pdfs/fourier.pdf', 'rb')
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-    print(pdfReader.numPages)
-    print(pdfReader.getPageLayout())
 
-    pageObj = pdfReader.getPage(pageNum)
-    print(pageObj.extractText())
+def processDocumentImage(imagePath):
+    # TODO: consider better file checking
+    if not imagePath.lower().endswith('.png'):
+        # convert to png
+        pass
+    
+    doc_image = Image.open(imagePath)
+    doc_text = pytesseract.image_to_string(doc_image)
+    var2def, var2ref = naive_parse_vars(doc_text) 
+    ret = redraw(doc_image, var2def, var2ref) 
+    return ret
 
-    pdfFileObj.close()
+
+def naive_parse_vars(text):
+    """ Naively parses document for variables defs and refs via template
+    matching.
+    :type text: str
+    :rtype: tuple( dict(str, str), dict(str, list[int]) )
+    """
+    keyphrases = {
+            "is denoted",
+            ""
+    }
+
+
+path = '../pdfs/fourier.png'
+processDocumentImage(path)
 
