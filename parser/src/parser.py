f091211a987b49403c7170ec2f90e59d18be19a8
shoyo-inokuchi <shoyoinokuchi@gmail.com>
2018-11-12 13:59:09 +0900

    added docstrings for parser.py

--- parser/src/parser.py
+++ parser/src/parser.py
@@ -1,32 +1,48 @@
 from pdf2image import convert_from_path
 import tempfile
-
 import pytesseract
 
 
 def processDocument(path):
     # TODO: consider better file checking
-    if not pdf_path.lower().endswith('.pdf'):
+    if not path.lower().endswith('.pdf'):
         raise Exception('File should be in PDF format')
 
-    ret = pdf_to_plaintext(path)
+    plaintxt = extract_PDFtext(path)
+    var2def = extract_vars(plaintxt)
 
 
-def pdf_to_plaintext(pdf_path):
+
+def extract_PDFtext(pdf_path):
+    """
+    Takes in path for PDF document and returns the text as a string.
+
+    :param str pdf_path: directory path of PDF document
+    :rtype str ret: concatenated string of all text in PDF document
+    """
     with tempfile.TemporaryDirectory() as path:
         doc_images = convert_from_path(pdf_path, output_folder=path, fmt='jpg')
 
-    for i in range(len(doc_images)):
-        doc_text = pytesseract.image_to_string(doc_images[i])
-        print(doc_text)
+        ret = ''
+        for i in range(len(doc_images)):
+            doc_text = pytesseract.image_to_string(doc_images[i])
+            ret += doc_text
+        return ret
 
 
-def parse_vars(text):
-    """ Naively parses document for variables defs and refs via template
-    matching.
-    :type text: str
-    :rtype: tuple( dict(str, str), dict(str, list[int]) )
+def extract_vars(text):
     """
+    Parses text for instances where variables are defined ("...x denotes...")
+    and returns a dictionary where defined variables are mapped to the
+    sentence that defines them.
+        Ex. "x denotes number of people." would be mapped as
+            {'x':'x denotes number of people.'}.
+
+    :param str text: extracted document text
+    :rtype dict[str, str] var2def: vars mapped to sentence that defined them
+    """
+    # TODO: handle documents where the same variable has multiple definitions.
+    # TODO: map words to their specific definition, not the whole sentence.
     keywords = {
         "denotes",
         "represents",
@@ -35,10 +51,13 @@ def parse_vars(text):
     }
 
     var2def = {}
+    buffer = ''
     
     for word in text:
         if word in keywords:
             pass
+
+    return var2def
 
 
 def redraw(image, var2def, var2ref):
@@ -49,8 +68,4 @@ def redraw(image, var2def, var2ref):
 if __name__ == '__main__':
     path = '../pdfs/fourier.pdf'
     processDocument(path)
-
-
-
-
 
