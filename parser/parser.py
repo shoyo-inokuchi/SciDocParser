6a7904e50d681df0ccecb5907a9f913ae4f6ad89
shoyo-inokuchi <shoyoinokuchi@gmail.com>
2018-11-25 20:53:32 +0900

    added scaffolding for processing functions exc. redraw

--- parser/parser.py
+++ parser/parser.py
@@ -1,6 +1,7 @@
+import sys
 from pdf2image import convert_from_path
-import tempfile
 import pytesseract
+import cv2
 
 
 def process_document(path):
@@ -8,30 +9,43 @@ def process_document(path):
     if not path.lower().endswith('.pdf'):
         raise Exception('File should be in PDF format')
 
-    plaintxt = extract_pdf_text(path)
+    doc_images = pdf_to_jpg(path)
+    plaintxt = extract_pdf_text(doc_images)
     var2def = extract_vars(plaintxt)
+    print(var2def)
 
 
-def extract_pdf_text(pdf_path):
+def pdf_to_jpg(pdf_path):
     """
-    Takes in path for PDF document and returns the text as a string.
-
+    Takes in path for PDF document and returns corresponding Image object.
     :param str pdf_path: directory path of PDF document
+    :rtype Image ret: Image object in jpg format
+    """
+    ret = convert_from_path(pdf_path, fmt='jpg')
+    if not ret:
+        print('Error converting path to jpg')
+        sys.exit()
+    return ret
+
+
+def extract_pdf_text(images):
+    """
+    Takes in PIL Image object and returns the contained text as a string.
+
+    :param str images: directory path of PDF document
     :rtype str ret: concatenated string of all text in PDF document
     """
-    with tempfile.TemporaryDirectory() as path:
-        doc_images = convert_from_path(pdf_path, output_folder=path, fmt='jpg')
-
-        ret = ''
-        for i in range(len(doc_images)):
-            doc_text = pytesseract.image_to_string(doc_images[i])
-            ret += doc_text
-        return ret
+    # TODO: be able to parse Greek symbols
+    ret = ''
+    for i in range(len(images)):
+        doc_text = pytesseract.image_to_string(images[i])
+        ret += doc_text
+    return ret
 
 
 def extract_vars(text):
     """
-    Parses text for instances where variables are defined ("...x denotes...")
+    (Naively) parses text for instances where variables are defined ("...x denotes...")
     and returns a dictionary where defined variables are mapped to the
     sentence that defines them.
         Ex. "x denotes number of people." would be mapped as
@@ -42,26 +56,48 @@ def extract_vars(text):
     """
     # TODO: handle documents where the same variable has multiple definitions.
     # TODO: map words to their specific definition, not the whole sentence.
+    # TODO: add recognition for mutliple vars in a sentence, add more robust var recog
+
     keywords = {
         "denotes",
         "represents",
         "means",
         "stands for",
+        "let",
+    }
+
+    common_vars = {
+        'x', 'y', 'z', 'a', 'b', 't'
     }
 
     var2def = {}
 
-    for sentence in text.split('.'):
-        if True:
-            pass
+    text = text.replace('\n', ' ')
+    for sentence in text.split('. '):
+        var_candidate = None
+        contains_keyword = False
+        for word in sentence.split():
+            if word in common_vars:
+                var_candidate = word
+            if word in keywords:
+                contains_keyword = True
+        if var_candidate and contains_keyword:
+            var2def[var_candidate] = sentence
 
     return var2def
 
 
-def redraw(image, var2def, var2ref):
-    pass
+def redraw(image, var2def):
+    """
+
+    :param PIL Image image:
+    :param dict[str, str] var2def:
+    :rtype ??? processed_image:
+    :rtype
+    """
+    cv2.matchTemplate(image, )
 
 
 if __name__ == '__main__':
-    path = '../pdfs/fourier.pdf'
-    ret = process_document(path)
+    path = 'pdfs/test1.pdf'
+    pd = process_document(path)
