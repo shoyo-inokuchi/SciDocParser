7f8b978934dd2b31af88a9365a3ce881de321286
shoyo-inokuchi <shoyoinokuchi@gmail.com>
2018-11-28 13:07:27 +0900

    parser ignore check

--- sdp_main/SciDocParser/settings.py
+++ sdp_main/SciDocParser/settings.py
@@ -123,8 +123,3 @@ STATIC_URL = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static/"),
 ]
-
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-]
